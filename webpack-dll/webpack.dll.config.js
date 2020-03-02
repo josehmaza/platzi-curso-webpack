4418c2c02ed03990b60520475116123d91b905f1
josehmaza <josehmaza@gmail.com>
2020-03-01 21:31:02 -0500

    Añadiendo un Dynamix Link Library

---
+++ webpack-dll/webpack.dll.config.js
@@ -0,0 +1,28 @@
+//common js
+const path = require('path')
+
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        //se generara un bundle modules
+        modules: [
+            'react',
+            'react-dom'
+        ]
+    },
+    mode: 'production',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js', // exportara como modules.js,
+        library: '[name]' // va a se una libreria
+    },
+
+    plugins: [
+       new webpack.DllPlugin({
+           name: '[name]',
+           path: path.join(__dirname, '[name]-manifest.json')
+       })
+    ],
+
+}
